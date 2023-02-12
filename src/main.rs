0e8984947e69dcb3efbb563fdaca5630136a3aba
y <3371348169@qq.com>
2023-02-12 20:57:19 +0800

    3.txt

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,12 @@ use walkdir::WalkDir;
 #[structopt(name = "file hasher", about = "Calculate hash.")]
 struct Opt {
     /// Set worker threads
-    // we don't want to name it "speed", need to look smart
     #[structopt(short = "t", long = "threads", default_value = "16")]
     threads: usize,
+
+    /// Set file minium size in 3.txt
+    #[structopt(short = "s", long = "size", default_value = "1048576")]
+    min_size: usize,
 }
 
 fn main() {
@@ -21,15 +24,25 @@ fn main() {
         .create(true)
         .append(false)
         .write(true)
+        .truncate(true)
         .open("./1.txt")
         .unwrap();
     let mut out2 = fs::OpenOptions::new()
         .create(true)
         .append(false)
         .write(true)
+        .truncate(true)
         .open("./2.txt")
         .unwrap();
+    let mut out3 = fs::OpenOptions::new()
+        .create(true)
+        .append(false)
+        .write(true)
+        .truncate(true)
+        .open("./3.txt")
+        .unwrap();
     let mut can_decrease_size = 0_usize;
+    let mut can_decrease_size_large_file = 0_usize;
     for (k, v) in &map {
         out1.write(format!("{}\n{}\n", k.0, k.1).as_bytes())
             .unwrap();
@@ -48,7 +61,20 @@ fn main() {
             out2.write(format!("{}\n", s).as_bytes()).unwrap();
         }
         out2.write("\n".as_bytes()).unwrap();
+
+        if k.1 < opt.min_size {
+            continue;
+        }
+        can_decrease_size_large_file += (v.len() - 1) * k.1;
+        out3.write(format!("{}\n{}\n", k.0, k.1).as_bytes())
+            .unwrap();
+        for s in v {
+            out3.write(format!("{}\n", s).as_bytes()).unwrap();
+        }
+        out3.write("\n".as_bytes()).unwrap();
     }
     out2.write(format!("{}\n", can_decrease_size).as_bytes())
+        .unwrap();
+    out3.write(format!("{}\n", can_decrease_size_large_file).as_bytes())
         .unwrap();
 }
